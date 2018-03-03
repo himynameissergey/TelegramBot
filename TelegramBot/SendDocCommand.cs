df3dc6888f3c084acf9bcbab1ab76c9300f60b52
Ященко Сергей <dammit@live.ru>
2018-03-04 00:17:12 +0300

    1

--- TelegramBot/SendDocCommand.cs
+++ TelegramBot/SendDocCommand.cs
@@ -29,21 +29,6 @@ namespace TelegramBot
 
             try
             {
-                // чтение из файла
-                //FileStream fstream = System.IO.File.OpenRead(@"test");
-                //{
-                    // преобразуем строку в байты
-                    //byte[] array = new byte[fstream.Length];
-                    // считываем данные
-                    //fstream.Read(array, 0, array.Length);
-                    // декодируем байты в строку
-                    //string textFromFile = System.Text.Encoding.Default.GetString(array);
-                    //Console.WriteLine("Текст из файла: {0}", textFromFile);
-                    //FileToSend doc = new FileToSend("test", fstream);
-
-                    //await client.SendDocumentAsync(chatId, doc);
-                //}
-
                 var FileUrl = @"excel.xlsx";
                 using (var stream = System.IO.File.Open(FileUrl, FileMode.Open))
                 {
